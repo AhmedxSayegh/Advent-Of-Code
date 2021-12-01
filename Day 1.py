13d89d335ff4c74220e4b66a111168ddaa62d6ff
Ahmed Sayegh <86713746+AhmedxSayegh@users.noreply.github.com>
2021-12-01 23:02:42 +0300

    Add files via upload

---
+++ Day 1.py
@@ -0,0 +1,25 @@
+measurements = open('Text.txt').read().split('\n')
+measurements = [int(i) for i in measurements]
+total = 0
+window = 0
+windows = {}
+
+# Part 1
+for count, measurement in enumerate(measurements[1::]):
+    if measurement > measurements[count]:
+        total += 1
+print('Part 1 answer:', total)
+
+total = 0
+
+# Part 2
+for count in range(len(measurements)):
+    if count + 3 > len(measurements):
+        break
+    windows[window] = sum(measurements[count:count+3])
+    window += 1
+
+for i in range(1,len(windows)):
+    if windows[i] > windows[i-1]:
+        total += 1
+print('Part 2 answer:', total)
