d876fa4e05d88721ca77e05d843c02e8bcb6cdae
Ahmed Sayegh <86713746+AhmedxSayegh@users.noreply.github.com>
2021-12-25 22:41:15 +0300

    Update Day 1.py

--- 2021/Day 1.py
+++ 2021/Day 1.py
@@ -3,26 +3,21 @@ measurements = [int(i) for i in content]
 
 # Part 1
 total = 0
-
 for count, measurement in enumerate(measurements[1::]):
     if measurement > measurements[count]:
         total += 1
-
 print('Part 1 Answer:', total)
 
 # Part 2
 windows = {}
 total = 0
 window = 0
-
 for count in range(len(measurements)):
     if count + 3 > len(measurements):
         break
     windows[window] = sum(measurements[count:count + 3])
     window += 1
-
 for i in range(1, len(windows)):
     if windows[i] > windows[i - 1]:
         total += 1
-
 print('Part 2 Answer:', total)
