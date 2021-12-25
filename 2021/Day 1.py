fd0a95298237eb984662af5305b4d49089510318
Ahmed Sayegh <86713746+AhmedxSayegh@users.noreply.github.com>
2021-12-25 22:35:51 +0300

    Update Day 1.py

--- 2021/Day 1.py
+++ 2021/Day 1.py
@@ -1,18 +1,20 @@
 content = open('Input.txt').read().split('\n')
 measurements = [int(i) for i in content]
-total = 0
-windows = {}
 
 # Part 1
+total = 0
+
 for count, measurement in enumerate(measurements[1::]):
     if measurement > measurements[count]:
         total += 1
+
 print('Part 1 Answer:', total)
 
-
 # Part 2
+windows = {}
 total = 0
 window = 0
+
 for count in range(len(measurements)):
     if count + 3 > len(measurements):
         break
@@ -22,4 +24,5 @@ for count in range(len(measurements)):
 for i in range(1, len(windows)):
     if windows[i] > windows[i - 1]:
         total += 1
+
 print('Part 2 Answer:', total)
