4e5ba071a00f4cf77c575dc5edc63ecf8690a1af
Ahmed Sayegh <86713746+AhmedxSayegh@users.noreply.github.com>
2021-12-25 22:41:39 +0300

    Update Day 2.py

--- 2021/Day 2.py
+++ 2021/Day 2.py
@@ -3,7 +3,6 @@ content = open('Input.txt').read().split('\n')
 # Part 1
 ver = 0
 hor = 0
-
 for instruction in content:
     direction, value = instruction.split()[0], int(instruction.split()[-1])
     if direction.startswith('f'):
@@ -11,15 +10,13 @@ for instruction in content:
     elif direction.startswith('d'):
         ver += value
     else:
-        ver -= value
-        
+        ver -= value       
 print('Part 1 Answer:', ver * hor)
 
 # Part 2
 ver = 0
 hor = 0
 aim = 0
-
 for instruction in content:
     direction, value = instruction.split()[0], int(instruction.split()[-1])
     if direction.startswith('f'):
@@ -28,6 +25,5 @@ for instruction in content:
     elif direction.startswith('d'):
         aim += value
     else:
-        aim -= value
-        
+        aim -= value      
 print('Part 2 Answer:', ver * hor)
