b4bf7dedbbd06f46aad497d06eca9b1a3cdb41f5
Ahmed Sayegh <86713746+AhmedxSayegh@users.noreply.github.com>
2021-12-31 16:33:51 +0300

    Update Day 3.py

--- 2021/Day 3.py
+++ 2021/Day 3.py
@@ -1,14 +1,5 @@
 content = open('Input.txt').read().split('\n')
 
-
-# Used By Both Parts
-def binary_to_dicimal(num):
-    dicimal = 0
-    for count, digit in enumerate(num):
-        dicimal += int(digit) * 2 ** (len(num) - count - 1)
-    return dicimal
-
-
 # Part 1
 gamma_rate = ''
 for x in range(len(content[0])):
@@ -19,8 +10,8 @@ for x in range(len(content[0])):
         gamma_rate += '1'
     else:
         gamma_rate += '0'
-epsilon_rate = ''.join(list(map(lambda x: '1' if x is '0' else '0', gamma_rate)))
-print('Part 1 Answer:', binary_to_dicimal(gamma_rate) * binary_to_dicimal(epsilon_rate))
+epsilon_rate = ''.join(list(map(lambda q: '1' if q is '0' else '0', gamma_rate)))
+print('Part 1 Answer:', int(gamma_rate, 2) * int(epsilon_rate, 2))
 
 
 # Part 2
@@ -30,15 +21,15 @@ def ox_rating(report, index):
     else:
         zero_counter = 0
         one_counter = 0
-        for y in range(len(report)):
-            if report[y][index] == '1':
+        for w in range(len(report)):
+            if report[w][index] == '1':
                 one_counter += 1
             else:
                 zero_counter += 1
         if one_counter >= zero_counter:
-            ox_counter = list(filter(lambda xreport: xreport[index] == '1', report))
+            ox_counter = list(filter(lambda q: q[index] == '1', report))
         else:
-            ox_counter = list(filter(lambda xreport: xreport[index] == '0', report))
+            ox_counter = list(filter(lambda q: q[index] == '0', report))
         index += 1
         return ox_rating(ox_counter, index)
 
@@ -49,17 +40,17 @@ def co2_rating(report, index):
     else:
         zero_counter = 0
         one_counter = 0
-        for y in range(len(report)):
-            if report[y][index] == '1':
+        for i in range(len(report)):
+            if report[i][index] == '1':
                 one_counter += 1
             else:
                 zero_counter += 1
         if one_counter < zero_counter:
-            co2_counter = list(filter(lambda xreport: xreport[index] == '1', report))
+            co2_counter = list(filter(lambda q: q[index] == '1', report))
         else:
-            co2_counter = list(filter(lambda xreport: xreport[index] == '0', report))
+            co2_counter = list(filter(lambda q: q[index] == '0', report))
         index += 1
         return co2_rating(co2_counter, index)
 
 
-print('Part 2 Answer:', binary_to_dicimal(ox_rating(content, 0)) * binary_to_dicimal(co2_rating(content, 0)))
+print('Part 2 Answer:', int(ox_rating(content, 0), 2) * int(co2_rating(content, 0), 2))
